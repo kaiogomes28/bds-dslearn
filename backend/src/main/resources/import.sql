a86da2599481af71e0d20b2bfd8de12d0ee62909
Kaio Gomes <kaio.e.m.gomes@gmail.com>
2022-04-15 13:09:47 -0300

    Offer

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.business2community.com/wp-content/uploads/2015/07/Online-course_750x410-300x164.jpg.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSymEKhD9jpfPC5MvC51hYrHXkYyTIH1HkOMKRPf6MnWH5_fXD7T5mAFN6c4QYJyKGqrcA&usqp=CAU'); 
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-11-13T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-12-13T03:00:00Z', 1);
