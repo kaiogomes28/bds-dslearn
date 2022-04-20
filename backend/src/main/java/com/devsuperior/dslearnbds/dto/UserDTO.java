f99af2d4a21bcb53df5dad86141b6d604af12543
Kaio Gomes <kaio.e.m.gomes@gmail.com>
2022-04-20 08:42:59 -0300

    Custom service level authorization

--- backend/src/main/java/com/devsuperior/dslearnbds/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/dto/UserDTO.java
@@ -6,7 +6,7 @@ import com.devsuperior.dslearnbds.entities.User;
 
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+	 
 	private Long id;
 	private String name;
 	private String email;
